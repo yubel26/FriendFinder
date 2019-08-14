e553bb6ecf5310ea2fbc4f0048167fdccfa77acc
yubel26 <yubelribot26@gmail.com>
2019-08-14 17:29:55 -0400

    finished page missing to push to heroku

---
+++ README.md
@@ -0,0 +1,19 @@
+# FriendFinder
+Node and Express Servers Exercise
+
+#### Instructions
+1) clone repo
+2) run 'npm install'
+3) run program using 'npm start'
+
+
+
+
+#### Description
+Friend Finder will find the best match from a list of users based on results from the survey.
+User data will be saved to list of users for future matches
+
+#### Dependencies
+body-parser
+cor
+express
---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friendsArray = [
+    {
+        "name": "Arash",
+        "photo": "https://vignette.wikia.nocookie.net/fategrandorder/images/9/98/Arash4.png/revision/latest?cb=20170207175557",
+        "scores": ["5","1","4","4","5","1","2","5","4","1"]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": ["4","2","5","1","3","2","2","1","3","2"]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": ["5","2","2","2","4","1","3","2","5","5"]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": ["3","3","4","2","2","1","3","2", "2","3"]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://static1.fjcdn.com/comments/Iamthebestfapper+rolled+a+random+image+posted+in+comment+63+at+_a4096c284eba93cfb31486a84705bdb1.jpg",
+        "scores": ["4","3","4","1","5","2","5","3","1","4"]
+    },
+    {
+        "name": "WATSON AI",
+        "photo": "https://images.techhive.com/images/article/2017/01/1024px-ibm_watson-100632019-orig-100703361-large.jpg",
+        "scores": ["4","4","2","3","2","2","3","2","4","5"]
+    },
+    {
+        "name": "123",
+        "photo": "https://www.shareicon.net/data/128x128/2015/12/28/694274_numbers_512x512.png",
+        "scores": ["1","1","1","1","1","1","1","1","1","1"]
+    },
+    {
+        "name": "Kyle Harrison",
+        "photo": "efa",
+        "scores": ["1","2","5","3","4","5","1","3","2","3"]
+    }
+]
+
+module.exports = friendsArray;
